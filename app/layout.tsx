83996cd7ab645a4dca45c87c95a7da76625f15b5
Thawinwats Uggaravisee <thawinwats.ugg@satangcorp.com>
2022-10-31 16:23:15 +0700

    🎨 Adding tailwindcss

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
   return (
     <html>
       <head></head>
-      <body>{children}</body>
+      <body className="dark:bg-black dark:text-white">{children}</body>
     </html>
   )
 }
